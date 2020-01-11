7870521f3ae4325bdfa8699dd2377a29eec5b48d
mixheaven <mixheaven@hotmail.com>
2020-01-11 10:51:01 +0100

    final shoot

--- TestDate.java
+++ TestDate.java
@@ -1,3 +1,4 @@
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -9,26 +10,33 @@ class TestDate {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please fill a day (1-31) : " + c.get(Calendar.DAY_OF_MONTH));
+        System.out.println("Please fill a day (1-31) : ");
         int day = sc.nextInt();
-        System.out.println("Please fill a month (1-12) : " + c.get(Calendar.MONTH));
+        System.out.println("Please fill a month (1-12) : ");
         int month = sc.nextInt();
-        System.out.println("Please fill a year : " + c.get(Calendar.YEAR));
+        System.out.println("Please fill a year : ");
         int year = sc.nextInt();
         sc.close();
 
         // TODO : your code after this line
-        Date current = new date();
-        SimpleDateFormat format = new SimpleDateFormat('dd, MMM, yyyy');
-        try{
-
+        String stringDate = String.valueOf(month) + "-" + String.valueOf(day) + "-" + String.valueOf(year);
+        SimpleDateFormat simpleFormat = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = null;
+        try {
+            date = simpleFormat.parse(stringDate);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
 
-        Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int dayOfMonth = c.get(Calendar.DAY_OF_MONTH);
-        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        Calendar calendar = new GregorianCalendar(day, month - 1, year);
+
+        if (date != null) {
+            System.out.println("The date is : " + simpleFormat.format(date));
+            System.out.println("The day of week is :" + calendar.get(Calendar.DAY_OF_WEEK));
+        }
+        System.out.println("Fin !!");
 
     }
+
 }
